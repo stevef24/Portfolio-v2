1bfa02ac9c539ce0db479b416dae4686e2f7490c
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-08-06 22:36:59 +0100

    minor update to the section header page

--- my-app/components/About/About.tsx
+++ my-app/components/About/About.tsx
@@ -1,9 +1,25 @@
+import Reveal from "../reveal";
 import SectionHeader from "../sectionHeader";
 
 const About = () => {
 	return (
 		<>
-			<SectionHeader title={"About"} order={"flex-row"} />
+			<Reveal width="100%">
+				<SectionHeader title={"About"} order={"flex-row"} />
+			</Reveal>
+
+			<div className="grid">
+				<Reveal>
+					<p className="mt-6">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam et
+						impedit, laudantium aperiam modi quasi dignissimos recusandae,
+						ducimus excepturi illum voluptatum praesentium at, omnis incidunt
+						illo sunt expedita alias dolor error dolorem beatae maiores minus
+						asperiores? Odit expedita explicabo, minus corporis veritatis soluta
+						doloremque aperiam cum eaque culpa molestias fuga.
+					</p>
+				</Reveal>
+			</div>
 		</>
 	);
 };
