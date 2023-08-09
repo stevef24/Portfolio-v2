978cddaa98f0b90b63725d95be14ec76d72aa38a
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-08-09 22:23:16 +0100

    added contact page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/About/About";
 import Hero from "@/components/Hero/hero";
+import Contact from "@/components/Contact/Contact";
 
 const Home = () => {
 	return (
 		<main className="container">
 			<Hero />
 			<About />
+			<Contact />
 		</main>
 	);
 };
