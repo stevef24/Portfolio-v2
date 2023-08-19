0d6f1182a459d9067bf0b8af4b192f3e5734842e
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-08-19 21:01:09 +0100

    added projects page

--- my-app/components/Projects/Projects.tsx
+++ my-app/components/Projects/Projects.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import SectionHeader from "../sectionHeader";
 import Reveal from "../reveal";
+import HorizontalScrollCarousel from "../carousal/ProjectCarousal";
 
 type Props = {};
 
 const Projects = (props: Props) => {
 	return (
-		<>
+		<section>
 			<Reveal width="100%">
 				<SectionHeader title={"Projects"} order={"flex-row"} />
 			</Reveal>
-		</>
+			<HorizontalScrollCarousel />
+		</section>
 	);
 };
 
